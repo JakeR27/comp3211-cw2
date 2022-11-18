c46c7946ac6e0e23955384579893626a8a2e0afc
Jake BS <j.bailey-saltmarsh+github@outlook.com>
2022-11-18 13:31:11 +0000

    added comments

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
+// required imports
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// import routers from other files
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
+// create the server
 let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// specify middleware to use
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// catch requests to static paths
+app.use(express.static(path.join(__dirname, 'public')));
+// catch requests to / paths
 app.use('/', indexRouter);
+// catch requests to /users paths
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
