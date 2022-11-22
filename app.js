1c46c08cd9ff815b0412800f9ab949155b0a1db1
Jake BS <j.bailey-saltmarsh+github@outlook.com>
2022-11-22 12:04:58 +0000

    added cors and removed irrelevant imports

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors')
 
 // import routers from other files
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
 
 // create the server
 let app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 // specify middleware to use
 app.use(logger('dev'));
@@ -22,12 +18,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-// catch requests to static paths
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200,
+}))
+
 // catch requests to / paths
 app.use('/', indexRouter);
-// catch requests to /users paths
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
